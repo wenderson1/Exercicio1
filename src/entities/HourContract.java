e9a94905c08bd23c876356cdc12c3fcbaf90c97a
Wenderson Farias <wenderson.farias10@gmail.com>
2019-09-07 01:03:30 -0300

    Projeto terminado

---
+++ src/entities/HourContract.java
@@ -0,0 +1,52 @@
+package entities;
+import java.util.Date;
+
+public class HourContract {
+	
+	
+	private Date date;
+	private Double valuePerHour;
+	private Integer hours;
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+	
+	public HourContract() {
+		
+	}
+
+	public HourContract(Date date, double valuePerHour, int hours) {
+		
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+	
+	public double Totalvalue() {
+		return valuePerHour * hours;
+	}
+	
+	
+	
+}
---
+++ src/entities/WorkerLevel.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum WorkerLevel {
+	
+	
+	 JUNIOR, MID_LEVEL, SENIOR;
+	 
+
+}
